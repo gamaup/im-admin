c835aac189369583b5dad0029fb322cb84847b76
Gama Unggul Priambada <poonkcrinxz@gmail.com>
2014-10-23 18:08:23 +0700

    Added Standard Table

--- table.php
+++ table.php
@@ -19,9 +19,11 @@
                     <h3>Standard Table</h3>
                     <h5>Contoh tampilan table secara default</h5>
                 </div>
+<!--
                 <div class='page-action'>
                     <a href='#' class='button button-blue'><i class="fa fa-thumb-tack"></i> Tambah Data</a>
                 </div>
+-->
             </div>
             <div class='breadcrumb'>
                 <a href='#'>Dashboard</a><i class="fa fa-angle-right"></i><a href='#'>Table</a><i class="fa fa-angle-right"></i>Standard Table
@@ -32,9 +34,6 @@
                         <div class='panel-head'>
                             <h4>Default Table</h4>
                             <p>Table dengan bentuk default</p>
-                            <div class='panel-action'>
-                                <a href='#'><i class="fa panel-expand tipl" title="View All"></i></a>
-                            </div>
                         </div>
                         <div class='panel-body'>
                             <table>
@@ -76,6 +75,9 @@
                                 </tr>
                             </table>
                         </div>
+                        <div class='panel-footer'>
+                            Format standar untuk table
+                        </div>
                     </div>
                 </div>
                 <div class='col-2'>
@@ -83,9 +85,6 @@
                         <div class='panel-head'>
                             <h4>Bordered Table</h4>
                             <p>Table dengan border untuk pembatas antar cell</p>
-                            <div class='panel-action'>
-                                <a href='#'><i class="fa panel-expand tipl" title="View All"></i></a>
-                            </div>
                         </div>
                         <div class='panel-body'>
                             <table class='bordered'>
@@ -127,20 +126,66 @@
                                 </tr>
                             </table>
                         </div>
+                        <div class='panel-footer'>
+                            <p>Tambahkan class <code>bordered</code> pada tag <code>table</code></p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class='row'>
-                <div class='col-4'>
+                <div class='col-2'>
                     <div class='panel'>
                         <div class='panel-head'>
-                            <h5>Table With Action</h5>
-                            <div class='panel-action'>
-                                <a href='#'><i class="fa panel-expand tipl" title="View All"></i></a>
-                            </div>
+                            <h5>Opsi Warna</h5>
                         </div>
                         <div class='panel-body'>
-                            <table class='bordered table-yellow'>
+                            <table class='table-red'>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nama</th>
+                                    <th>NIM</th>
+                                    <th>Alamat</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Jon Snow</td>
+                                    <td>11523999</td>
+                                    <td>Jogja</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Daenerys Targaryen</td>
+                                    <td>11523888</td>
+                                    <td>Sleman</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Tyrion Lannister</td>
+                                    <td>11523777</td>
+                                    <td>Ngemplak</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-1'>
+                    <div class='panel-head'>
+                        <h5>Opsi Warna</h5>
+                    </div>
+                    <div class='panel-body'>
+                        <p>Terdapat 5 opsi warna yaitu Putih(default), Merah, Biru, Hijau, dan Kuning</p>
+                        <p>Tambahkan class <code>table-red</code> pada tag <code>table</code> untuk membuat warna merah, begitu juga untuk warna lain.</p>
+                    </div>
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col-3'>
+                    <div class='panel'>
+                        <div class='panel-head'>
+                            <h5>Buttons Inside Table</h5>
+                        </div>
+                        <div class='panel-body'>
+                            <table class='table-blue'>
                                 <tr>
                                     <th>ID</th>
                                     <th>Nama</th>
@@ -153,12 +198,9 @@
                                     <td>Jon Snow</td>
                                     <td>11523999</td>
                                     <td>Jogja</td>
-                                    <td class='action'>
-                                        <div class='action-set'>
-                                        <a href='#'><i class="fa fa-eye"></i>View</a>
-                                        <a href='#'><i class="fa fa-download"></i>Download</a>
-                                        <a href='#'><i class="fa fa-trash-o"></i>Delete</a>
-                                        </div>
+                                    <td>
+                                        <a href='#' class='button button-icon'><i class='fa fa-pencil'></i></a>
+                                        <a href='#' class='button button-icon'><i class='fa fa-trash-o'></i></a>
                                     </td>
                                 </tr>
                                 <tr>
@@ -166,10 +208,9 @@
                                     <td>Daenerys Targaryen</td>
                                     <td>11523888</td>
                                     <td>Sleman</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye"></i>View</a>
-                                        <a href='#'><i class="fa fa-download"></i>Download</a>
-                                        <a href='#'><i class="fa fa-trash-o"></i>Delete</a>
+                                    <td>
+                                        <a href='#' class='button button-icon'><i class='fa fa-pencil'></i></a>
+                                        <a href='#' class='button button-icon'><i class='fa fa-trash-o'></i></a>
                                     </td>
                                 </tr>
                                 <tr>
@@ -177,114 +218,32 @@
                                     <td>Tyrion Lannister</td>
                                     <td>11523777</td>
                                     <td>Ngemplak</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye"></i>View</a>
-                                        <a href='#'><i class="fa fa-download"></i>Download</a>
-                                        <a href='#'><i class="fa fa-trash-o"></i>Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>Hodor</td>
-                                    <td>11523666</td>
-                                    <td>Pamungkas</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye"></i>View</a>
-                                        <a href='#'><i class="fa fa-download"></i>Download</a>
-                                        <a href='#'><i class="fa fa-trash-o"></i>Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>5</td>
-                                    <td>White Walker</td>
-                                    <td>11523555</td>
-                                    <td>Harjobinangun</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye"></i>View</a>
-                                        <a href='#'><i class="fa fa-download"></i>Download</a>
-                                        <a href='#'><i class="fa fa-trash-o"></i>Delete</a>
+                                    <td>
+                                        <a href='#' class='button button-icon'><i class='fa fa-pencil'></i></a>
+                                        <a href='#' class='button button-icon'><i class='fa fa-trash-o'></i></a>
                                     </td>
                                 </tr>
                             </table>
                         </div>
                     </div>
                 </div>
+                <div class='col-1'>
+                    <div class='panel-head'>
+                        <h5>Button didalam table</h5>
+                    </div>
+                    <div class='panel-body'>
+                        <p>Tambahkan anchor dengan class <code>button</code> yang bisa dibaca penjelasannya di bagian UI Elements > Buttons</p>
+                    </div>
+                </div>
             </div>
             <div class='row'>
-                <div class='col-4'>
-                    <div class='panel'>
-                        <div class='panel-head'>
-                            <h5>Table With Action (Icon)</h5>
-                            <div class='panel-action'>
-                                <a href='#'><i class="fa panel-expand tipl" title="View All"></i></a>
-                            </div>
-                        </div>
-                        <div class='panel-body'>
-                            <table class='table-red bordered'>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nama</th>
-                                    <th>NIM</th>
-                                    <th>Alamat</th>
-                                    <th>Action</th>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Jon Snow</td>
-                                    <td>11523999</td>
-                                    <td>Jogja</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye tipt" title='View'></i></a>
-                                        <a href='#'><i class="fa fa-download tipt" title="Download"></i></a>
-                                        <a href='#'><i class="fa fa-trash-o tipt" title="Delete"></i></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Daenerys Targaryen</td>
-                                    <td>11523888</td>
-                                    <td>Sleman</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye tipt" title='View'></i></a>
-                                        <a href='#'><i class="fa fa-download tipt" title="Download"></i></a>
-                                        <a href='#'><i class="fa fa-trash-o tipt" title="Delete"></i></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>Tyrion Lannister</td>
-                                    <td>11523777</td>
-                                    <td>Ngemplak</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye tipt" title='View'></i></a>
-                                        <a href='#'><i class="fa fa-download tipt" title="Download"></i></a>
-                                        <a href='#'><i class="fa fa-trash-o tipt" title="Delete"></i></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>Hodor</td>
-                                    <td>11523666</td>
-                                    <td>Pamungkas</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye tipt" title='View'></i></a>
-                                        <a href='#'><i class="fa fa-download tipt" title="Download"></i></a>
-                                        <a href='#'><i class="fa fa-trash-o tipt" title="Delete"></i></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>5</td>
-                                    <td>White Walker</td>
-                                    <td>11523555</td>
-                                    <td>Harjobinangun</td>
-                                    <td class='action'>
-                                        <a href='#'><i class="fa fa-eye tipt" title='View'></i></a>
-                                        <a href='#'><i class="fa fa-download tipt" title="Download"></i></a>
-                                        <a href='#'><i class="fa fa-trash-o tipt" title="Delete"></i></a>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
+                <div class='col-2'>
+                    <div class='panel-head'>
+                        <h5>Opsi Lain</h5>
+                    </div>
+                    <div class='panel-body'>
+                        <p>Tambahkan class <code>paddingless</code> pada element di dalam <code>table</code> untuk menghilangkan padding.</p>
+                        <p>Tambahkan class <code>center</code> pada element di dalam <code>table</code> untuk membuat teks berada di tengah.</p>
                     </div>
                 </div>
             </div>
